3af720836b4b36045c9d9729200c817c427986a0
Wired361 <justin.parker.12@gmail.com>
2019-08-18 21:04:15 -0500

    fixed issue with duplicates and single activity push

--- class-helper.sh
+++ class-helper.sh
@@ -27,26 +27,27 @@ popd () {
 
 # Check repo changes before doing anything
 function gitchecker () {
-   echo "Checking for changes to Lesson Plan"
-   pushd $DATAVIZ; diffchecker "Lesson Plan"; popd
-   pushd $CLASSREPO; diffchecker "Class Repo"; popd
+   echo "Checking status of Repositories"
+   pushd $DATAVIZ; diffchecker "Lesson Plan"; 
+   #pushd $CLASSREPO; diffchecker "Class Repo"; 
+   
 }
 
 # Quietly check for differences in local and remote repos
 function diffchecker () {
    if [[ `git diff HEAD origin/master` ]]; then
-      echo $1 is up to date
+      echo "$1 is up to date"
+      
    else
       # Changes
-      read -r -p "$1 has changes, pull down changes? [Y/N]" lessonpull
+      read -r -p "$1 has changes, pull down changes? [Y]/N: " lessonpull
+
       case $lessonpull in
-         [Yy]* ) 
+         [Nn]* )
+         ;;
+         [Yy]* | *) 
             git pull -q origin master;      
          ;;
-         [Nn]* )
-            topicfind;;
-         * )
-            exit;;
       esac
    fi
 }
@@ -58,13 +59,14 @@ function diffchecker () {
 
 # Prompt for weekly topic and pass 
 function topicfind () {
+   pushd $CLASSREPO/Class-MW-Rice-Cookers
    # Topic prompt
-   read -r -p "What's the topic this week?: " topic        
-   topicpath="`find -L $DATAVIZ/01-Lesson-Plans -maxdepth 1 -iname *$topic* -type d -print`" 
+   read -r -p "What's the topic this week?: "  topic;
+   topicpath="`find -L $DATAVIZ/01-Lesson-Plans -maxdepth 1 -iname *$topic* -type d -print -quit`" 
    
    # Handler for errors or spelling
    if test -z "$topicpath"; then
-      echo "$topic lesson not found, try again"
+      echo  $topic lesson not found, try again
       topicfind $1
    else
       basetopic=$(basename $topicpath)
@@ -74,18 +76,18 @@ function topicfind () {
 
 function copy2class () {
    # Output directory name and confirm prompt
-   read -r -p "$(echo $basetopic)? [Y/N] " input 
+   read -r -p "$(echo $basetopic)? [Y]/N: " input 
    case $input in
-      [Yy]* ) 
-         echo "Copying 01-Lesson-Plans/$basetopic --> $CLASSREPO";
-         cp -r $topicpath $CLASSREPO;
-         saveyourself
-         homeworkhandler;;
       [Nn]* ) 
-         topicfind;;
-      * ) 
-         exit;;
+         topicfind copy2class;;
+      [Yy]* | * ) 
+         echo "Copying 01-Lesson-Plans/$basetopic --> /Class-MW-Rice-Cookers/$basetopic";
+   
+         cp -r $topicpath $CLASSREPO/Class-MW-Rice-Cookers;
+         saveyourself;
+         dailysetup;;
    esac
+   
 }
 
 ############################
@@ -94,8 +96,8 @@ function copy2class () {
 
 # Remove instruction-team only materials
 function saveyourself () {
-   pushd $CLASSREPO/$basetopic
-   echo Cleaning up
+   pushd $CLASSREPO/Class-MW-Rice-Cookers/$basetopic
+   echo 'Cleaning up'
    rm -r -f readme.md VideoGuide.md Supplemental
    for x in 1 2 3
       do
@@ -104,7 +106,7 @@ function saveyourself () {
       done
    echo "Creating weekly .gitignore"
    find 1 2 3 -maxdepth 3 -type d | sort | grep -v \"un\|Reg\" > .gitignore
-   popd
+   
 }
 
 #######################
@@ -112,53 +114,83 @@ function saveyourself () {
 #######################
 
 function homeworkhandler () {
-   read -r -p "Do you want to add the homework? [Y/N] " homework   
+   read -r -p "Do you want to add the homework? [Y]/N " homework   
    case $homework in
-      [Yy]* )
+      [Nn]* ) 
+         echo "Have a great class!";;
+      [Yy]* | * )
          echo "Copying 02/Homework/$basetopic --> /HW/$basetopic"
          mkdir $CLASSREPO/HW/$basetopic
          cp -r $DATAVIZ/02-Homework/$basetopic/Instructions/* $CLASSREPO/HW/$basetopic
          pushd $CLASSREPO/HW/$basetopic
          echo "Cleaning up"
          rm -r -f Solutions gradingrubric.md gradingrubrics      
-         popd;;
-      [Nn]* ) 
-         echo "Have a great class!";;
-      * ) 
-         exit;;
+         ;;
    esac
+   echo "Have a great day!"
 }
 
 
 ##############################
-#    Activity Pushing        #
+#        Daily Setup         #
 ##############################
 
-# Currently Set up to push out all unsolved
-# Single Activities is on the docket
-function activitypusher () {
-   pushd $CLASSREPO/$basetopic
-   echo Working from $basetopic directory
+function dailysetup () {
+   pushd $CLASSREPO/Class-MW-Rice-Cookers/$basetopic
+   echo "Working from $basetopic directory"
    echo "Which Lecture Day?"
-   echo "[1]  [2]  [3]  [4]: all" 
-   read -r day
-   case $day in
-      [4] )
-         masspush
-         ;;
-       *)
+   read -r -p "[1]  [2]  [3] : " day
+   read -r -p "Mass Comment Unsolved? [Y]/N: " massconfirm
+   case $massconfirm in
+      [nN]* )
+         echo "Moving to Activity Pusher";
+         activitypusher;;
+      [Yy]* | * )
          echo "Commenting $basetopic Day $day Activities"
          masspush
          echo "Done"
-         echo "Have a great class!"
-         ;;
+         activitypusher;;
    esac
-      
+   
 }
 
 function masspush () {
-   sed -i "/^.*Solved/! s/^$day/#$day/g" $CLASSREPO/$basetopic/.gitignore
+   sed -i "/^.*Solved/! s/^$day/#$day/" $CLASSREPO/Class-MW-Rice-Cookers/$basetopic/.gitignore
 }
+
+##############################
+#      Activity Pusher       #
+##############################
+
+function activitypusher () {
+   pushd $CLASSREPO/Class-MW-Rice-Cookers/$basetopic
+   linecount=0
+   for line in $(cat .gitignore)
+   do
+   ((linecount++))
+      if [[ $line =~ ^$day.*Solved ]]; then
+         read -r -p "$(echo $line | cut -d'/' -f 3)? [Y]/N: " linecheck
+         case $linecheck in
+            [Nn]* ) 
+               continue;;
+            [Yy] | * )
+               sed -i "$linecount {s/^/#/}" .gitignore
+               echo "Commented $line"
+               continue;;
+         esac
+      else
+         continue
+      fi
+   done
+   echo "No more lessons in Lecture $day"
+   # Test for homework directory, if false trigger homework handler
+   if [ -d "$CLASSREPO/HW/$basetopic" ]; then
+      echo "Have a great day!"
+   else
+      homeworkhandler;
+   fi
+}
+
 
 ##############################
 #     Use Case Prompt        #
@@ -167,9 +199,9 @@ function masspush () {
 # Allow for different use cases
 # Weekly Class Setup, Homework, etc...
 
-echo "What do you want to do?"
-echo "[1]: Weekly Setup [2]: Homework [3]: Push Activities"
-read -r usecase
+echo -e "What do you want to do?"
+read -r -p "[1]-Weekly Setup [2]-Homework [3]-Activities : " usecase
+
 case $usecase in
    [1] ) 
       gitchecker;
@@ -179,8 +211,8 @@ case $usecase in
       topicfind homeworkhandler;;
    [3] )
       gitchecker;
-      topicfind activitypusher;;
+      topicfind dailysetup;;
    * )
       echo "Please choose a valid selection" 
-      class-helper.sh;;
+      helper;;
 esac
